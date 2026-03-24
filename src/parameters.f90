1f913273f94bbcd008251f6874e521c9c5ad378f
Itxaso Muñoz <itxaso.mam@gmail.com>
2026-03-24 11:06:43 +0100

    updating parameters

--- src/parameters.f90
+++ src/parameters.f90
@@ -1,10 +1,10 @@
 module GLOBAL 
   implicit none 
-  integer:: d, L, N, z, NT, nsw, seed ! Simulation parameters
-  integer(kind=8):: nMCS, nmeas ! MC steps and MC measurement step number
-  double precision:: Tmin, Tmax ! Temperature range
-  double precision, allocatable:: temp_list(:), beta_list(:) ! Lists of T and betas
-! character(len=64):: bond_mode ! Nearest or all
+  integer:: d, L, N, z, NT, nsw, seed 
+  integer(kind=8):: nMCS, nmeas 
+  double precision:: Tmin, Tmax 
+  double precision, allocatable:: temp_list(:), beta_list(:) 
+ character(len=64):: bond_mode ! Nearest or all
 contains
 
 ! Read input parameters from a file
@@ -12,31 +12,33 @@ contains
     implicit none
     character(*), intent(in):: inputfile
     open(10, file=trim(inputfile), status='old') 
-    read(10,*) d 
-    read(10,*) L 
-    read(10,*) Tmin 
-    read(10,*) Tmax 
-    read(10,*) NT 
-    read(10,*) nsw 
-    read(10,*) nmeas 
-    read(10,*) nMCS 
-    read(10,*) seed 
-!    read(10,'(A)') bond_mode ! New
+    read(10,*) d                    ! space dimension
+    read(10,*) L                    ! linear size of the lattice     
+    read(10,*) Tmin                 ! min temp
+    read(10,*) Tmax                 ! max temp
+    read(10,*) NT                   ! num of replicas (1 for each T)
+    read(10,*) nsw                  ! num of MC steps between swap attempts
+    read(10,*) nmeas                ! num of measurement steps
+    read(10,*) nMCS                 ! num of total MCsteps 
+    read(10,*) seed                 ! rng seed
+    read(10,'(A)') bond_mode        ! bond type: gaussian or ferro
     close(10) 
-!    bond_mode = adjustl(trim(bond_mode)) ! New
+   bond_mode = adjustl(trim(bond_mode)) 
   end subroutine read_input
 
 ! Initialize global variables and allocate T, beta lists
   subroutine init_globals() 
     implicit none 
-    integer:: k, idir
+    integer:: k
 
-    !N = 1 
-    !do idir = 1, d 
-    !  N = N*L 
-    !enddo 
+    ! Total num of spins in the lattice:
+    N = L**d
+    print*, 'N=', N
 
-    !z = 2*d 
+    ! Coordination number:
+    ! Num of nn for square/cubic lattices with pbcs
+    z = 2*d 
+    print*, 'z=', z
 
     ! The arrays' dimensions are NT: number of replicas.
     ! Tk are linearly spaced between Tmin and Tmax.
@@ -51,6 +53,10 @@ contains
     do k = 1, NT 
       beta_list(k) = 1.d0/temp_list(k) 
     enddo 
+
+    print*, 'temp_list=', temp_list
+    print*, 'beta_list=', beta_list
+
   end subroutine init_globals 
 
 end module GLOBAL 
